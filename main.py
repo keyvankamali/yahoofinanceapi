19b08dd5e640540a4a8057a9e46c1fc4dd2d2437
Keyvan <keyvan.kamali.s@gmail.com>
2021-10-20 18:54:47 -0400

    added comments

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import time
 import calendar
 
 
+# get symbol name from the user
 print("Please enter symbol name:")
 symbol = input()
 
 gmt = time.gmtime()
 date = calendar.timegm(gmt)
 
+# url of data for a particular stock
 url = f"https://rest.yahoofinanceapi.com/v7/finance/options/{symbol}"
 
+# query parameters
 query_string = {
     "date": date
 }
 
+# our api-key
 headers = {
     'x-api-key': "hNcYRonIR289365RnQK21aYvNCZ2cHL1aITV9jKB"
 }
 
+# request to the api
 response = requests.request("GET", url, headers=headers, params=query_string)
 
 try:
